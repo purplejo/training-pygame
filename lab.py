ceb83b550759093ac555322efcde14cf6df84ba2
purplejo <purplejo.online@gmail.com>
2018-08-25 20:33:04 +0200

    update lab to quickly test last menus model

--- lab.py
+++ lab.py
@@ -16,8 +16,10 @@ screen = softwares.Screen(title="Lab")
 keyboard = softwares.Keyboard()
 mouse = softwares.Mouse()
 
-option_A = menus.Option(message="OPTION A", action=lambda: print("It's ALIVE!!!"))
-option_B = menus.Option(pos=(0, 100), message="OPTION B", previous_option=option_A, next_option=option_A)
+option_A = menus.Option(message="OPTION A")
+option_B = menus.Option(message="OPTION B", previous_option=option_A, next_option=option_A)
+
+main_menu = menus.Menu(options=[option_A, option_B])
 
 while screen.running:
     events = pygame.event.get()
@@ -27,25 +29,8 @@ while screen.running:
 
     if keyboard.push(pg.K_ESCAPE):
         screen.running = False
-    if keyboard.push('+', 99):
-        print("+")
 
-    if mouse.move():
-        for option in [option_A, option_B]:
-            if mouse.inside(option.area):
-                option.onfocus()
-                break
-            else:
-                option.onblur()
-
-    if mouse.push(1, 99):
-        for option in [option_A, option_B]:
-            if mouse.inside(option.area):
-                option.apply()
-                break
-
-    option_A.blit_on(screen)
-    option_B.blit_on(screen)
+    main_menu.loop()
 
     pygame.display.update()
     clock.tick(120)
